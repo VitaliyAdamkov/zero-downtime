cbe7dc5f705805ab55d7aa14bd3e6e828fcfc6bf
adamkov <adamkov@tex.ua>
2021-05-13 14:36:06 +0300

    Add zero-downtime scheme

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,13 +6,13 @@ set :repo_url, 'git@github.com:VitaliyAdamkov/zero-downtime.git'
 
 namespace :deploy do
   after :published, 'docker:create_network'
-
-  # Start nginx-proxy container
   after :published, 'docker:start_nginx'
-
   after :published, 'docker:create_volumes'
   after :published, 'docker:db_setup'
   after :published, 'docker:start_container'
+  after :published, 'docker:wait_for_container'
+  after :published, 'docker:stop_container'
+  after :published, 'docker:rename_container'
 end
 
 # Default branch is :master
